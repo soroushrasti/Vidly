b82844347f0fc9d5ef9d2c2260a10db81c38e1dd
Soroush Rasti <so.rasty@gamail.com>
2021-01-08 18:03:04 +0100

    authentication login

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import LoginForm from "./components/loginForm";
 import RegisterForm from "./components/registerForm";
+import Logout from "./components/Logout"
 import jwtDecode from 'jwt-decode'
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css' 
@@ -34,6 +35,7 @@ class App extends Component {
           <Switch>
             <Route path="/register" component={RegisterForm} />
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
--- src/services/authService.js
+++ src/services/authService.js
@@ -3,9 +3,14 @@ import config from '../config.json'
 
 const apiEndPoint=config.apiUrl+'/auth'
 
-export function login(email,password){
+export async function login(email,password){
+    const {data:jwt}= await login(email,password)
+    localStorage.setItem('token',jwt)
     return http.post( apiEndPoint,{
         email,
         password,
     })
+    export function logout(){
+        localStorage.removeItem('token')
+    }
 }
